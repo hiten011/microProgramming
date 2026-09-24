6e85a94509e79d3b3ae4418744b78afda3bd4e2a
hiten <hitengupta12345@gmail.com>
2026-09-24 18:10:17 +0930

    refactor(assignment1): use RGB enum names, not magic numbers

--- assignment1/Src/assignment1/my_helpers.c
+++ assignment1/Src/assignment1/my_helpers.c
@@ -1,4 +1,5 @@
 #include "my_helpers.h"
+#include "assignment_1.h"
 
 static void Pin_Set_If(GPIO_TypeDef *port, uint8_t pin, bool condition) {
     if (condition) {
@@ -9,15 +10,15 @@ static void Pin_Set_If(GPIO_TypeDef *port, uint8_t pin, bool condition) {
 }
 
 void Set_Truth_RGB(ItemColour_t truth_colour) {
-    Pin_Set_If(GPIOA, 0, truth_colour == 1);
-    Pin_Set_If(GPIOA, 1, truth_colour == 2);
-    Pin_Set_If(GPIOA, 4, truth_colour == 3);
+    Pin_Set_If(GPIOA, 0, truth_colour == RED);
+    Pin_Set_If(GPIOA, 1, truth_colour == GREEN);
+    Pin_Set_If(GPIOA, 4, truth_colour == BLUE);
 }
 
 void Set_Sensor_RGB(ItemColour_t sensor_colour) {
-    Pin_Set_If(GPIOA, 3, sensor_colour == 1);
-    Pin_Set_If(GPIOA, 11, sensor_colour == 2);
-    Pin_Set_If(GPIOA, 8, sensor_colour == 3);
+    Pin_Set_If(GPIOA, 3, sensor_colour == RED);
+    Pin_Set_If(GPIOA, 11, sensor_colour == GREEN);
+    Pin_Set_If(GPIOA, 8, sensor_colour == BLUE);
 }
 
 void Set_Fault(PinState st) {
